8cc8f4b42463f58f71364c4551da09eba5dd9e2d
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-07-31 18:14:26 +0200

    Added beforeAll method and beforeEach method. Added todo method on last test as reminder

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -3,8 +3,19 @@ import { Utils } from "../app/Utils";
 
 describe("Utils test suite", () => {
 
+    // will run one time before anything else runs
+    beforeAll(() => {
+        console.log("beforeAll");
 
-    
+    })
+
+
+    // will run before each test 
+    beforeEach(() => {
+        console.log("beforeEach");
+
+    })
+
     test("first test", () => {
         const result = Utils.toUpperCase("abc")
         expect(result).toBe("ABC")
@@ -17,15 +28,17 @@ describe("Utils test suite", () => {
         expect(parsedUrl.protocol).toBe("http:")
         expect(parsedUrl.query).toEqual({})
     })
-    
+
     test("Parse URL with query", () => {
         const parsedUrl = Utils.parseUrl("http://localhost:8080/login?user=user&password=pass")
         const expectedQuery = {
             user: "user",
-            password :"pass"
+            password: "pass"
         }
         expect(parsedUrl.query).toEqual(expectedQuery)
         expect(expectedQuery).toBe(expectedQuery)
 
     })
+    // reminds me to still write the test 
+    test.todo("test invalid URL")
 })
