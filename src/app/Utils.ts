f1c2939aadea409338898e1cb94e6d7b1f755b4e
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-07-31 17:53:24 +0200

    wrote logic to pass tests

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,8 +1,15 @@
+import { UrlWithParsedQuery, parse } from "url";
 
 
 export class Utils {
+    
+    public static parseUrl(url:string):UrlWithParsedQuery{
 
-    public static toUpperCase(arg: string) {
-        return ""
+        return parse(url, true)
+        
+    }
+
+    public static toUpperCase(arg: string): string {
+        return arg.toUpperCase()
     }
 }
